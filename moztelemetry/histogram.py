786b1d84c75f892c55023168af24f74069c2be42
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-01-06 17:32:01 +0000

    Add histogram utilities.

---
+++ moztelemetry/histogram.py
@@ -0,0 +1,42 @@
+from __future__ import division
+
+import requests
+import histogram_tools
+import pandas as pd
+import numpy as np
+
+_definitions = requests.get("https://hg.mozilla.org/mozilla-central/raw-file/2a193b7f395c/toolkit/components/telemetry/Histograms.json").json()
+
+class Histogram:
+    def __init__(self, name, instance):
+        if isinstance(instance, list):
+            assert(False) # minimal format not supported yet
+
+        entries = {int(k): v for k, v in instance["values"].items()}
+        self.definition = histogram_tools.Histogram(name, _definitions[name])
+        self.buckets = pd.Series(entries, index=self.definition.ranges()).fillna(0)
+
+    def get_values(self):
+        return self.buckets
+
+    def get_definition(self):
+        return self.definition
+
+    def percentile(self, percentile):
+        fraction = percentile/100
+        to_count = fraction*self.buckets.sum()
+        percentile_bucket = 0
+
+        for percentile_bucket in range(len(self.buckets)):
+            freq = self.buckets.values[percentile_bucket]
+            if to_count - freq <= 0:
+                break
+            to_count -= freq
+
+        if percentile_bucket == 0 or percentile_bucket == len(self.buckets) - 1:
+            return float('nan')
+
+        percentile_frequency = self.buckets.values[percentile_bucket]
+        percentile_lower_boundary = self.buckets.index[percentile_bucket]
+        width = self.buckets.index[percentile_bucket + 1] - self.buckets.index[percentile_bucket]
+        return percentile_lower_boundary + width*to_count/percentile_frequency
