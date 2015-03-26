1cff5701fbca81b4b9e7e78d16a87dd1b2fcbc49
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-03-26 17:35:25 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+ffrom distutils.core import setup
 
 setup(name='python_moztelemetry',
-      version='0.3.0.2',
+      version='0.3.0.3',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Spark bindings for Mozilla Telemetry',
