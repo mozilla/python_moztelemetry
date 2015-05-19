ed9102a282764ef7f93aa4562db65ab39936e349
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-05-19 17:04:12 +0100

    Bump up version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class FetchExternal(setuptools.command.install.install):
 
 setup(cmdclass={'install': FetchExternal},
       name='python_moztelemetry',
-      version='0.3.2.7',
+      version='0.3.2.8',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Spark bindings for Mozilla Telemetry',
