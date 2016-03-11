ce8bf67eb355e9898bd05d07d6fa9f29e6bc1735
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-03-11 13:11:49 +0000

    Limit maximum parallelism level for get_pings.
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1255730.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class FetchExternal(install):
 
 setup(cmdclass={'install': FetchExternal},
       name='python_moztelemetry',
-      version='0.3.9.1',
+      version='0.3.9.2',
       author='Roberto Agostino Vitillo',
       author_email='rvitillo@mozilla.com',
       description='Spark bindings for Mozilla Telemetry',
