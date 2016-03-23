86d777adabe65e902bb67337136bb50b116fbac7
Sam Penrose <spenrose@mozilla.com>
2016-03-23 15:36:27 -0700

    another test

--- test/test_standards.py
+++ test/test_standards.py
@@ -11,6 +11,8 @@ class TestStandards(unittest.TestCase):
 
         ping = {'clientId': '6fd3eb50-8bec-4b9c-8778-59406171312a'}
         self.assertRaises(ValueError, standards.in_sample, ping, 3)
+        for divisor in (10, 100, 1000):
+            self.failIf(standards.in_sample(ping, divisor))
 
         ping = {'clientId': '42fe5ab0-9b19-4d75-a9f1-0d57aada05d6'}
         for divisor in (10, 100, 1000):
