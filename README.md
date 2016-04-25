e0051d5062bc31affab4303ab6740148563c5c63
Mauro Doglio <mdoglio@mozilla.com>
2016-04-25 19:56:57 +0100

    Bug 1257551 - Add script to update histogram_tools

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ password:example_pass
 ```
 - Fetch the latest code with `git pull`
 - Update PyPI with `python setup.py sdist upload`
+
+
+## Updating histogram_tools.py
+moztelemetry/histogram_tools.py is a mirror of its counterpart from
+[mozilla-central](https://hg.mozilla.org/mozilla-central/raw-file/tip/toolkit/components/telemetry/histogram_tools.py).
+To update it to the latest version you can run
+```bash
+bin/update_histogram_tools
+```
--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 from setuptools import setup
 
+
 setup(
-      name='python_moztelemetry',
-      version='0.3.9.8',
-      author='Roberto Agostino Vitillo',
-      author_email='rvitillo@mozilla.com',
-      description='Spark bindings for Mozilla Telemetry',
-      url='https://github.com/vitillo/python_moztelemetry',
-      packages=['moztelemetry'],
-      package_dir={'moztelemetry': 'moztelemetry'},
-      install_requires=['boto', 'backports.lzma', 'ujson', 'requests', 'protobuf', 'expiringdict', 'functools32', 'py4j', 'pandas>=0.14.1', 'numpy>=1.8.2', 'joblib', 'telemetry-tools'])
+    name='python_moztelemetry',
+    version='0.3.9.8',
+    author='Roberto Agostino Vitillo',
+    author_email='rvitillo@mozilla.com',
+    description='Spark bindings for Mozilla Telemetry',
+    url='https://github.com/vitillo/python_moztelemetry',
+    packages=['moztelemetry'],
+    package_dir={'moztelemetry': 'moztelemetry'},
+    install_requires=['boto', 'backports.lzma', 'ujson', 'requests', 'protobuf', 'expiringdict', 'functools32', 'py4j', 'pandas>=0.14.1', 'numpy>=1.8.2', 'joblib', 'telemetry-tools'],
+    setup_requires = ['pytest-runner'],
+    tests_require = ['pytest'],
+)
