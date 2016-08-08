43760baea0f7b1f80b1fabfff09c424863268b73
Mauro Doglio <mdoglio@mozilla.com>
2016-08-08 17:40:06 +0100

    Bump up package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='python_moztelemetry',
-    version='0.3.10',
+    version='0.4.0',
     author='Roberto Agostino Vitillo',
     author_email='rvitillo@mozilla.com',
     description='Spark bindings for Mozilla Telemetry',
