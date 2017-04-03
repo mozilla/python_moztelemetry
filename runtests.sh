f223abaa59777f2893576872506f50e70d0464a1
William Lachance <wlachance@mozilla.com>
2017-04-03 16:32:14 -0400

    tweaks

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# if we are not inside the docker container, run this command *inside* the
+# docker container
+if [ ! -f /.dockerenv ]; then
+    docker run moztelemetry_docker ./runtests.sh
+    exit $?
+fi
+
 # Start hbase
 /hbase-$HBASE_VERSION/bin/start-hbase.sh
 /hbase-$HBASE_VERSION/bin/hbase-daemon.sh start thrift
 
 # Run tests
 coverage run --source=moztelemetry setup.py test --addopts "-v --timeout=60"
-coveralls
