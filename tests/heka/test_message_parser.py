2d607b07c1f46bd92579b6ba4bc0e35995e185c0
William Lachance <wlachance@mozilla.com>
2017-04-20 13:35:58 -0400

    Bug 1346312 - Parametrize heka message parsing tests

--- tests/heka/test_message_parser.py
+++ tests/heka/test_message_parser.py
@@ -1,78 +1,66 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import copy
 import json
+import pytest
+from google.protobuf.message import DecodeError
+from mock import MagicMock
 from moztelemetry.heka import message_parser
 from moztelemetry.util.streaming_gzip import streaming_gzip_wrapper
 
 
-def test_unpack(data_dir):
-    for t in ["plain", "snappy", "mixed", "gzip", "gzip_mixed"]:
-        filename = "{}/test_{}.heka".format(data_dir, t)
-        with open(filename, "rb") as o:
-            if "gzip" in t:
-                o = streaming_gzip_wrapper(o)
-            msg = 0
-            for r, b in message_parser.unpack(o, try_snappy=True):
+@pytest.mark.parametrize("heka_format,try_snappy,strict,expected_count,expected_exception", [
+    # snappy disabled
+    ("plain", False, False, 10, False),
+    ("snappy", False, False, 0, False),
+    ("mixed", False, False, 5, False),
+    ("gzip",  False, False, 10, False),
+    ("gzip_mixed", False, False, 5, False),
+    # snappy enabled
+    ("plain", True, False, 10, False),
+    ("snappy", True, False, 10, False),
+    ("mixed", True, False, 10, False),
+    ("gzip",  True, False, 10, False),
+    ("gzip_mixed", True, False, 10, False),
+    # strict mode on
+    ("plain", False, True, 10, False),
+    ("snappy", False, True, 0, True),
+    ("mixed", False, True, 5, True),
+    ("gzip",  False, True, 10, False),
+    ("gzip_mixed", False, True, 5, True)
+])
+def test_unpack(data_dir, heka_format, try_snappy, strict, expected_count,
+                expected_exception):
+    count = 0
+    threw_exception = False
+    filename = "{}/test_{}.heka".format(data_dir, heka_format)
+    with open(filename, "rb") as o:
+        if "gzip" in heka_format:
+            o = streaming_gzip_wrapper(o)
+        try:
+            for r, b in message_parser.unpack(o, try_snappy=try_snappy, strict=strict):
                 j = json.loads(r.message.payload)
-                assert msg == j["seq"]
-                msg += 1
-            assert 10 == msg
+                assert count == j["seq"]
+                count += 1
+        except DecodeError:
+            threw_exception = True
 
-
-def test_unpack_nosnappy(data_dir):
-    expected_counts = {"plain": 10, "snappy": 0, "mixed": 5,
-                       "gzip": 10, "gzip_mixed": 5}
-    for t in expected_counts.keys():
-        count = 0
-        filename = "{}/test_{}.heka".format(data_dir, t)
-        with open(filename, "rb") as o:
-            if "gzip" in t:
-                o = streaming_gzip_wrapper(o)
-            try:
-                for r, b in message_parser.unpack(o, try_snappy=False):
-                    count += 1
-            except:
-                pass
-            assert expected_counts[t] == count
-
-
-def test_unpack_strict(data_dir):
-    expected_exceptions = {"plain": False, "snappy": True, "mixed": True,
-                           "gzip": False, "gzip_mixed": True}
-    for t in expected_exceptions.keys():
-        count = 0
-        filename = "{}/test_{}.heka".format(data_dir, t)
-        threw = False
-        with open(filename, "rb") as o:
-            if "gzip" in t:
-                o = streaming_gzip_wrapper(o)
-            try:
-                for r, b in message_parser.unpack(o, strict=True, try_snappy=False):
-                    count += 1
-            except Exception:
-                threw = True
-        assert expected_exceptions[t] == threw
+    assert count == expected_count
+    assert threw_exception == expected_exception
 
 
 top_keys = set(["application", "clientId", "creationDate", "environment", "id", "meta",
-                "payload", "type", "version"])
+               "payload", "type", "version"])
 payload_keys = set(["UIMeasurements", "addonDetails", "childPayloads", "chromeHangs",
                     "fileIOReports", "histograms", "info", "keyedHistograms", "lateWrites",
                     "log", "processes", "simpleMeasurements", "slowSQL", "threadHangStats",
                     "ver", "webrtc"])
-
-
-def test_telemetry(data_dir):
-    filename = "{}/test_telemetry_gzip.heka".format(data_dir)
+@pytest.mark.parametrize("heka_format", ["snappy", "gzip"])
+def test_parse_heka_message(data_dir, heka_format):
+    filename = "{}/test_telemetry_{}.heka".format(data_dir, heka_format)
     with open(filename, "rb") as o:
         for r in message_parser.parse_heka_message(streaming_gzip_wrapper(o)):
-            assert set(r.keys()) == top_keys
-            assert set(r["payload"].keys()) == payload_keys
-
-    filename = "{}/test_telemetry_snappy.heka".format(data_dir)
-    with open(filename, "rb") as o:
-        for r in message_parser.parse_heka_message(o):
             assert set(r.keys()) == top_keys
             assert set(r["payload"].keys()) == payload_keys
 
