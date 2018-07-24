3a53727b8a393f1a8eb7937df91c60599ba4251d
Erin Cole <ecole@mozilla.com>
2018-07-24 10:04:23 -0700

    add unit tests for new function

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -9,7 +9,8 @@ from concurrent import futures
 import pytest
 
 import moztelemetry
-from moztelemetry.dataset import Dataset, _group_by_size_greedy
+from moztelemetry.dataset import Dataset, _group_by_size_greedy,
+                                _group_by_equal_size
 from moztelemetry.store import InMemoryStore, S3Store
 
 
@@ -235,6 +236,28 @@ def test_group_by_size_greedy():
         [{'size': 4}, {'size': 1}],
         [{'size': 3}],
         [{'size': 2}]
+    ]
+
+
+def test_group_by_equal_size(): 
+    obj_list1 = [dict(size=i) for i in range(1, 5)]
+    obj_list2 = [{'size':70}, {'size':70}, {'size':70}, {'size':70}]
+    obj_list3 = [{'size':4}, {'size':1}, {'size':3}, {'size':2}]
+
+    groups = _group_by_equal_size(obj_list1, 1)
+    assert groups == [
+            [{'size': 4}, {'size': 3}, 
+            {'size': 2}, {'size': 1}]
+    ]
+    groups = _group_by_equal_size(obj_list2, 2, 100)
+    assert groups == [ 
+        [(70, {'size': 70}), (70, {'size': 70})], 
+        [(70, {'size': 70}), (70, {'size': 70})]
+    ]
+    groups = _group_by_equal_size(obj_list3, 3, 5)
+    assert groups == [
+        [(1, {'size': 1}), (2, {'size': 2}), (3, {'size': 3})], 
+        [(4, {'size': 4})]
     ]
 
 
