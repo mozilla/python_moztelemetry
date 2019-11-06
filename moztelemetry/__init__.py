3a1f932844d8a88d3739e7c5f1a66d3cbbde06b3
William Lachance <wlachance@mozilla.com>
2019-11-06 14:02:10 -0500

    Add a deprecation notice when people try to import this library

--- moztelemetry/__init__.py
+++ moztelemetry/__init__.py
@@ -1,3 +1,12 @@
 # flake8: noqa
 from .spark import *
 from .histogram import *
+
+
+print(
+    "WARNING: python_moztelemetry will stop working *VERY SOON* "
+    "(if it hasn't stopped working already)\n"
+    "Please see this post to fx-data-dev for more "
+    "information: "
+    "https://mail.mozilla.org/pipermail/fx-data-dev/2019-November/000291.html"
+)
